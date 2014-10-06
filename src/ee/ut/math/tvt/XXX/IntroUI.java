23efb86f0ccfedf2195a5c967185c8b4d65bf78e
dimazzy <dimazzy@gmail.com>
2014-10-06 14:19:18 +0300

    introui javafx

--- src/ee/ut/math/tvt/XXX/IntroUI.java
+++ src/ee/ut/math/tvt/XXX/IntroUI.java
@@ -1,5 +1,13 @@
 package ee.ut.math.tvt.XXX;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 public class IntroUI {
 	public static void FirstMessage() {
 		System.out.println("Tere!");
@@ -16,9 +24,42 @@ Your software version number
 		String name = "XXX";
 		String leader = "???";
 		String leader_email = "leader@gmail.com";
-		String members = "Ingvar-J�rgen Kurruk, Dmitri Janis, Allan Tsakuhhin";
+		String members = "Ingvar-Jörgen Kurruk, Dmitri Janis, Allan Tsakuhhin";
 		
 		String software_version = "Java SE Development Kit 8u20";
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage){
+		GridPane gp=new GridPane();
+		Label tName=new Label("Team name: XXX");
+		Label tLeader=new Label("Team leader: Allan Tsakuhhin");
+		String email=("allan.tsakuhhin@gmail.com");
+		Label tEmail= new Label("Team leader email: "+ email);
+		String members=("Allan Tsakuhhin,"+" Ingvar-Jörgen Kurruk,"+" Dmitri Janis");
+		Label tMembers= new Label("Team members: "+ members);
+		Image img=new Image("http://www.seguinsheep.com/Triple_20X_1_.jpg");
+		ImageView iv=new ImageView(img);
+		iv.setFitWidth(500);
+		iv.setFitHeight(200);
+		String sfNr=("");
+		Label sNr=new Label("Software version number: "+sfNr);
+		
+		gp.add(tName,0,0);
+		gp.add(tLeader,0,1);
+		gp.add(tEmail, 0, 2);
+		gp.add(tMembers,0,3);
+		gp.add(iv,0,4);
+		gp.add(sNr,0,5);
+		
+		Scene scene=new Scene(gp,500,320);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Intro");
+		primaryStage.show();
 		
 	}
 }
