756c1cf7ec3d6f75fd5cad1761c09cbee0006b11
Tsakuhhin <allan.tsakuhhin@gmail.com>
2014-10-06 13:44:12 +0300

    New Intro and IntroUI

--- src/ee/ut/math/tvt/XXX/Intro.java
+++ src/ee/ut/math/tvt/XXX/Intro.java
@@ -1,11 +1,10 @@
 package ee.ut.math.tvt.XXX;
 
-
 public class Intro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		IntroUI.FirstMessage();
+		
+
 	}
 
 }
--- src/ee/ut/math/tvt/XXX/IntroUI.java
+++ src/ee/ut/math/tvt/XXX/IntroUI.java
@@ -2,6 +2,23 @@ package ee.ut.math.tvt.XXX;
 
 public class IntroUI {
 	public static void FirstMessage() {
-		System.out.println("Tere");
+		System.out.println("Tere!");
+		
+		/*
+		Team name
+Team leader
+Team leader email
+Team members
+Team logo (random image)
+Your software version number
+		 */
+		
+		String name = "XXX";
+		String leader = "???";
+		String leader_email = "leader@gmail.com";
+		String members = "Ingvar-Jörgen Kurruk, Dmitri Janis, Allan Tsakuhhin";
+		
+		String software_version = "Java SE Development Kit 8u20";
+		
 	}
 }
