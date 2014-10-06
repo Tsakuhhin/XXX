6f5054ff7d3ce4375f0c44b86c34ef9456d01d1e
dimazzy <dimazzy@gmail.com>
2014-10-06 14:24:00 +0300

    update

--- src/ee/ut/math/tvt/XXX/IntroUI.java
+++ src/ee/ut/math/tvt/XXX/IntroUI.java
@@ -8,27 +8,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class IntroUI {
-	public static void FirstMessage() {
-		System.out.println("Tere!");
-		
-		/*
-		Team name
-Team leader
-Team leader email
-Team members
-Team logo (random image)
-Your software version number
-		 */
-		
-		String name = "XXX";
-		String leader = "???";
-		String leader_email = "leader@gmail.com";
-		String members = "Ingvar-Jörgen Kurruk, Dmitri Janis, Allan Tsakuhhin";
-		
-		String software_version = "Java SE Development Kit 8u20";
-		
-	}
+public class IntroUI extends Application{
+
 	
 	public static void main(String[] args) {
 		launch(args);
