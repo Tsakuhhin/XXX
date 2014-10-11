0838d87f0afd1eddb7f04abcdddd3706e26beda8
1n6v2r <ingvar.jorgen@gmail.com>
2014-10-12 00:57:40 +0300

    - Added version.properties content - Added build version reading from file - application.properties file reading fix - Uncommented close button part

--- src/ee/ut/math/tvt/XXX/IntroUI.java
+++ src/ee/ut/math/tvt/XXX/IntroUI.java
@@ -39,51 +39,51 @@ public class IntroUI extends Application {
 		GridPane gp = new GridPane();
 		gp.setStyle("-fx-border-insets: 23; -fx-background-insets: 23;-fx-background-radius: 6;-fx-border-radius: 6;-fx-border-color: gray;-fx-border-style: solid;-fx-border-width: 1;-fx-effect: dropshadow(three-pass-box, rgba(100, 100, 100, 1), 24, 0.5, 0, 0);");
 
-		/*
-		Label tName = new Label("Team name: XXX");
-		Label tLeader = new Label("Team leader: Allan Tsakuhhin");
-		String email = ("allan.tsakuhhin@gmail.com");
-		Label tEmail = new Label("Team leader email: " + email);
-		String members = ("Allan Tsakuhhin," + " Ingvar-Jorgen Kurruk,"
-				+ " Dmitri Janis");
-		Label tMembers = new Label("Team members: " + members);
-		*/
-		
 		// failist application.properties andmete lugemine
-		BufferedReader reader = new BufferedReader (new FileReader("application.properties"));
-		StringBuilder out = new StringBuilder();
-		String line = reader.readLine();
-		while (line != null){
-			int i = 1;
-			out.append(line);
-			out.append(System.lineSeparator());
-			Label data = new Label(out.toString());
+		String line = "";
+		File vfile = new File("application.properties");
+		FileReader vfileReader = new FileReader(vfile);
+		BufferedReader vbufferedReader = new BufferedReader(vfileReader);
+		StringBuffer vstringBuffer = new StringBuffer();
+		int i = 0;
+		while ((line = vbufferedReader.readLine()) != null) {
+			Label data = new Label(line + "\n");
 			gp.add(data, 0, i);
-			line = reader.readLine();
-			i = i+1;	
+			i++;
 		}
-        reader.close();
-		
+		vfileReader.close();
         
 		Image img = new Image("file:resources/xxx.jpg");
 		ImageView iv = new ImageView(img);
 		iv.setFitWidth(500);
 
 		iv.setFitHeight(120);
-		sfNr = (""); // get from file
+		
+		// get  build.number from version.properties
+		sfNr = null; 
+		File file = new File("version.properties");
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		StringBuffer stringBuffer = new StringBuffer();
+		while ((line = bufferedReader.readLine()) != null) {
+			String[] split = line.split("=");
+			if (split[0].equalsIgnoreCase("build.number")) {
+				sfNr = split[1];
+			}
+		}
+		fileReader.close();		
+		
 		Label sNr = new Label("Software version number: " + sfNr);
 		Button close = new Button("close");
 		close.setStyle("-fx-text-fill: #242d35;-fx-font-size: 10px;-fx-background-insets: 0 0 0 0,0,0;");
 
-		/*
+		
 		close.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				primaryStage.close();
 			}
 		});
-		/*
-		 
 		
 		/*
 		gp.add(info, 0, 1);
