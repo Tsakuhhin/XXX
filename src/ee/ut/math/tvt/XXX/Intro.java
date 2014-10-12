84147f4be2916b9ea311288156e3dc3cc848ae8f
dimazzy <dimazzy@gmail.com>
2014-10-12 19:57:49 +0300

    log4j + methods

--- src/ee/ut/math/tvt/XXX/Intro.java
+++ src/ee/ut/math/tvt/XXX/Intro.java
@@ -1,8 +1,11 @@
 package ee.ut.math.tvt.XXX;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Intro {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		
 		// runs class "IntroUI"
         IntroUI.main(args);
--- src/ee/ut/math/tvt/XXX/IntroUI.java
+++ src/ee/ut/math/tvt/XXX/IntroUI.java
@@ -2,10 +2,17 @@ package ee.ut.math.tvt.XXX;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import sun.misc.IOUtils;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,48 +29,70 @@ import javafx.stage.StageStyle;
 
 public class IntroUI extends Application {
 	private String sfNr;
+	static Logger log = Logger.getLogger(IntroUI.class.getName());
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
+		
+		// setup properties for log4j logger
+		Properties props = new Properties();
+		props.load(new FileInputStream("lib/log4j.properties"));
+		PropertyConfigurator.configure(props);
+		log.info("Launching IntroUI.");
+		
 		launch(args);
+
 	}
 
-	public void start(final Stage primaryStage) throws IOException {
+	public void start(Stage primaryStage) throws IOException {
 		BorderPane bp = new BorderPane();
-
 		GridPane gp = new GridPane();
 		gp.setStyle("-fx-border-insets: 23; -fx-background-insets: 23;-fx-background-radius: 6;-fx-border-radius: 6;-fx-border-color: gray;-fx-border-style: solid;-fx-border-width: 1;-fx-effect: dropshadow(three-pass-box, rgba(100, 100, 100, 1), 24, 0.5, 0, 0);");
 
-		// read data from file named "application.properties"
-		String line = "";
-		File vfile = new File("application.properties");
-		FileReader vfileReader = new FileReader(vfile);
-		BufferedReader vbufferedReader = new BufferedReader(vfileReader);
-		StringBuffer vstringBuffer = new StringBuffer();
-		int i = 0;
-		while ((line = vbufferedReader.readLine()) != null) {
-			Label data = new Label(line + "\n");
-			gp.add(data, 0, i);
-			i++;
-		}
-		vfileReader.close();
-        
-		// Into windows background logo
+		// Intro windows logo
 		Image img = new Image("file:resources/xxx.jpg");
 		ImageView iv = new ImageView(img);
 		iv.setFitWidth(500);
 		iv.setFitHeight(120);
-		
-		// get  build.number from "version.properties"
-		sfNr = null; 
-		File file = new File("version.properties");
-		FileReader fileReader = new FileReader(file);
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
-		StringBuffer stringBuffer = new StringBuffer();
-		
-		// get software version number parts
+
+		// get build.number from "version.properties"
+		sfNr = buildNumber();
+		Label sNr = new Label("Software version number: " + sfNr);
+		Button close = new Button("close");
+		close.setStyle("-fx-text-fill: #242d35;-fx-font-size: 10px;-fx-background-insets: 0 0 0 0,0,0;");
+
+		// Window close action
+		close.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				primaryStage.close();
+			}
+		});
+
+		// window properties
+		gp.add(iv, 0, 0);
+		gp.add(introData(), 0, 1);
+		gp.add(sNr, 0, 2);
+		bp.setCenter(gp);
+		bp.setTop(close);
+		BorderPane.setAlignment(close, Pos.TOP_RIGHT);
+
+		Scene scene = new Scene(bp, 550, 270);
+		primaryStage.setScene(scene);
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.setTitle("Intro");
+		primaryStage.show();
+	}
+
+	// get build nr
+	private String buildNumber() throws IOException {
+		String line = "";
 		String buildRevNum = "";
 		String buildMinNum = "";
 		String buildMajNum = "";
+		File file = new File("version.properties");
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+
 		while ((line = bufferedReader.readLine()) != null) {
 			String[] split = line.split("=");
 			if (split[0].equalsIgnoreCase("build.revision.number")) {
@@ -75,33 +104,26 @@ public class IntroUI extends Application {
 			}
 			sfNr = buildMajNum + "." + buildMinNum + "." + buildRevNum;
 		}
-		fileReader.close();		
-		
-		Label sNr = new Label("Software version number: " + sfNr);
-		Button close = new Button("close");
-		close.setStyle("-fx-text-fill: #242d35;-fx-font-size: 10px;-fx-background-insets: 0 0 0 0,0,0;");
-
-		// Window close action
-		close.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-				primaryStage.close();
-			}
-		});
-		
-		// window properties
-		gp.add(iv, 0, 0);
-		gp.add(sNr, 0, 5);
-		bp.setCenter(gp);
-		bp.setTop(close);
-		BorderPane.setAlignment(close, Pos.TOP_RIGHT);
-
-		Scene scene = new Scene(bp, 550, 270);
-		primaryStage.setScene(scene);
-		primaryStage.initStyle(StageStyle.TRANSPARENT);
-		primaryStage.setTitle("Intro");
-		primaryStage.show();
+		fileReader.close();
+		return sfNr;
 	}
-	
 
-	
+	// get data from application.prop
+	private GridPane introData() throws IOException {
+		GridPane iGP = new GridPane();
+		String line = "";
+		File vfile = new File("application.properties");
+		FileReader vfileReader = new FileReader(vfile);
+		BufferedReader vbufferedReader = new BufferedReader(vfileReader);
+		int i = 0;
+		while ((line = vbufferedReader.readLine()) != null) {
+			Label data = new Label(line + "\n");
+			iGP.add(data, 0, i);
+			i++;
+		}
+		vfileReader.close();
+
+		return iGP;
+	}
+
 }
